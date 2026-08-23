abfe0c742b357ff595d9d2a7d5e53de624ec7588
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2026-08-23 07:17:16 +0700

    chore: initialize spring boot project

---
+++ src/main/java/com/amru/assetmaintenance/AssetMaintenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amru.assetmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetMaintenanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetMaintenanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amru/assetmaintenance/AssetMaintenanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amru.assetmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetMaintenanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
